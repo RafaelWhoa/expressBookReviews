851e194bd31b8cf8815b2715adaa0464b2e411c7
Rafael Costa <44804@tjmt.jus.br>
2023-11-23 13:04:32 -0400

    feat: add get book review method

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,8 +53,10 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params["isbn"]
+    let book = books[isbn]
+    let bookReviews = book["reviews"]
+    return res.status(300).json(bookReviews);
 });
 
 module.exports.general = public_users;
