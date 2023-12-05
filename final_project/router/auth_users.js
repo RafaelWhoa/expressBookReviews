664e4c3a301953ef49091fc3b72ac55dadc2f47b
Rafael Costa <44804@tjmt.jus.br>
2023-12-05 19:44:36 -0400

    feat: Create delete a book by isbn method

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -58,6 +58,20 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     }
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params["isbn"]
+    const book = books[isbn]
+    const reviewByUser = Object.values(book.reviews).filter((review) => review.username === req.user.username)[0]
+    if (reviewByUser !== undefined) {
+        book.reviews = Object.values(book.reviews).filter((review) => review.username !== req.user.username)
+        res.status(200).json({message: "Review deleted successfully", deletedReview: reviewByUser})
+    }
+    else {
+        res.status(404).json({message: "Book review not found for the user"})
+    }
+})
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
